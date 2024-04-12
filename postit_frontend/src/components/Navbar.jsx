2fa3d6cdeca0794063e0bb05f45fce89980ebf99
Berlin Bruno <berlinbruno112@gmail.com>
2024-04-12 20:55:04 +0530

    Link

--- postit_frontend/src/components/Navbar.jsx
+++ postit_frontend/src/components/Navbar.jsx
@@ -21,10 +21,10 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
         />
       </div>
       <div className="flex gap-3">
-        <Link to={`user-profile/${user?._id}`} className="hidden md:block">
+        <Link to={`/user-profile/${user?._id}`} className="hidden md:block">
           <img src={user.image} alt="user" className="w-14 h-12 rounded-md" />
         </Link>
-        <Link to='create-pin' className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:f-12 flex justify-center items-center">
+        <Link to='/create-pin' className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:f-12 flex justify-center items-center">
           <IoMdAdd/>
         </Link>
       </div>
